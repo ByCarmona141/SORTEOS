2ffec840d8a106f67137f4f62c7ea64112b89af5
ByCarmona141 <ByCarmona141@gmail.com>
2026-04-07 15:01:46 -0600

    ADD: Agregadas rutas de prueba

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\TestMailController;
+use App\Http\Controllers\TestReportController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -23,6 +25,16 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/auth/me', [AuthController::class, 'me']);
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     Route::post('/auth/refresh', [AuthController::class, 'refreshToken']);
+
+    // emails
+    Route::get('/test-email-blade', [TestMailController::class, 'blade']);
+    Route::get('/test-email-markdown', [TestMailController::class, 'markdown']);
+    Route::get('/test-email-simple', [TestMailController::class, 'simple']);
+    Route::get('/test-email-with-attachment', [TestMailController::class, 'withAttachment']);
+    Route::get('/test-email-with-pdf', [TestMailController::class, 'withPdf']);
+
+    // pdf
+    Route::get('/test-pdf', [TestReportController::class, 'generatePdf']);
 });
 
 
